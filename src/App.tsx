39e1b53c680e0a4391ae48a88062e6b9fed983fd
Evgenyj <worker_lab@mail.ru>
2022-01-04 20:30:40 +0300

    Todolist-Tuesday-1-progress-addTask

--- src/App.tsx
+++ src/App.tsx
@@ -43,10 +43,10 @@ function App() {
         setTasks({...tasks, [todolistID]:tasks[todolistID].filter(t => t.id != id)})
     }
 
-    function addTask(title: string) {
-        /*let task = {id: v1(), title: title, isDone: false};
-        let newTasks = [task, ...tasks];
-        setTasks(newTasks);*/
+    function addTask(todolistID: string, title: string) {
+        let newTask = {id: v1(), title: title, isDone: false};
+        setTasks({...tasks, [todolistID]:[newTask,...tasks[todolistID]]})
+
     }
 
     function changeStatus(taskId: string, isDone: boolean) {
