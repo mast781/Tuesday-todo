1612c1c324050285f778d5cc56f8d156a1846232
Evgenyj <worker_lab@mail.ru>
2022-01-04 20:15:18 +0300

    Todolist-Tuesday-1-progress-removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -39,9 +39,8 @@ function App() {
     });
 
 
-    function removeTask(id: string) {
-        /*let filteredTasks = tasks.filter(t => t.id != id);
-        setTasks(filteredTasks);*/
+    function removeTask(todolistID: string, id: string) {
+        setTasks({...tasks, [todolistID]:tasks[todolistID].filter(t => t.id != id)})
     }
 
     function addTask(title: string) {
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -11,7 +11,7 @@ type PropsType = {
     todolistID: string
     title: string
     tasks: Array<TaskType>
-    removeTask: (taskId: string) => void
+    removeTask: (todolistID:string, taskId: string) => void
     changeFilter: (todolistID: string, value: FilterValuesType) => void
     addTask: (title: string) => void
     changeTaskStatus: (taskId: string, isDone: boolean) => void
@@ -62,7 +62,7 @@ export function Todolist(props: PropsType) {
         <ul>
             {
                 props.tasks.map(t => {
-                    const onClickHandler = () => props.removeTask(t.id)
+                    const onClickHandler = () => props.removeTask(props.todolistID, t.id)
                     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
                         props.changeTaskStatus(t.id, e.currentTarget.checked);
                     }
